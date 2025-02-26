df61a7f896bf48859cdab56266e3db688a017305
hungbin <bincoitk31@gmail.com>
2025-02-26 17:34:21 +0700

    Fix

---
+++ Makefile
@@ -0,0 +1,2 @@
+deploy:
+	ansible-playbook -i ansible/inventory.yaml ansible/deploy.yaml;
--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Page, Swiper, Box, Text } from "zmp-ui";
+import settings from '../../app-settings.json'
 
-const Carousel = ({images} = props) => {
+const Carousel = () => {
+  const SLIDERS = settings ?.sliders || []
   return (
     <Swiper autoplay duration={5000} loop>
       {
-        images.map((el, idx) => (
+        SLIDERS.map((el, idx) => (
           <Swiper.Slide key={idx}>
             <img
               className="slide-img h-[200px] w-full object-cover"
-              src={el}
+              src={el.url}
               alt={`slide-${idx}`}
             />
           </Swiper.Slide>
